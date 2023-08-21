28d3d79eac0dd0165e294fbc06703e89bd888ae1
vladshakhtar <stolyarovs2003@gmail.com>
2023-08-21 20:25:08 +0300

    Initial Commit

---
+++ MacPawTestApp/MacPawTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacPawTestAppApp.swift
+//  MacPawTestApp
+//
+//  Created by Vladislav Stolyarov on 21.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MacPawTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
